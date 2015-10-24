a4c703580b2ed6d62431e539100d0c433bc74f0b
Saqib Kayani <m.s.kayani@edu.salford.ac.uk>
2015-10-24 21:15:14 +0100

    fixed issue with the header overlapping all the content.

--- www/js/app.js
+++ www/js/app.js
@@ -3,12 +3,20 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('App', ['ionic'])
+
+
+.controller('ToDoCtrl',function($scope) {
+    $scope.tasks = [];
+
+    $scope.doNewTask = function() {
+      $window.alert("Hi");  
+    };
+};
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
@@ -17,3 +25,23 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+
+
+//navigation
+app.config(function($stateProvider) {
+  $stateProvider
+  .state('index', {
+    url: '/',
+    templateUrl: 'home.html'
+  })
+  .state('music', {
+    url: '/music',
+    templateUrl: 'music.html'
+  });
+});
+
+
+
+
+
