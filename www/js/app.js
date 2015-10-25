a244c8912f085a34263e0c1d42c10d2e0d8e25da
Saqib Kayani <m.s.kayani@edu.salford.ac.uk>
2015-10-25 01:30:29 +0100

    ADD controller for button

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,9 @@
 var stealApp = angular.module('App', ['ionic'])
 
 stealApp.controller("CrimeCtrl", function($scope, $http) {
-  $http.get('http://stealapi.apphb.com/api/Crime/GetARandomCrime').
+
+    $scope.getData = function(){
+        $http.get('http://stealapi.apphb.com/api/Crime/GetARandomCrime').
     success(function(data, status, headers, config) {
       $scope.crimes = data;
       console.log(data);
@@ -14,6 +16,7 @@ stealApp.controller("CrimeCtrl", function($scope, $http) {
     error(function(data, status, headers, config) {
       // log error
     });
+  }
 });
 
 
@@ -35,14 +38,6 @@ stealApp.controller("CrimeCtrl", function($scope, $http) {
 stealApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
 
-      $http.get('http://stealapi.apphb.com/api/Crime/GetARandomCrime').
-    success(function(data, status, headers, config) {
-      $scope.crimes = data;
-      console.log(data);
-    }).
-    error(function(data, status, headers, config) {
-      // log error
-    });
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
